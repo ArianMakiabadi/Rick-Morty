85d0c780ee9282767e2241e3b8d56086f5270682
arian <arian.maki@yahoo.com>
2025-08-17 17:31:21 +0200

    CharacterList added (static)

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,15 @@
 import "./App.css";
+import CharacterList from "./components/CharacterList";
 import Navbar from "./components/Navbar";
+import { allCharacters } from "../data/data";
 
 function App() {
   return (
     <div className="app">
       <Navbar />
+      <div className="main">
+        <CharacterList allCharacters={allCharacters} />
+      </div>
     </div>
   );
 }
