730c20ca5ee9d5643c67725ebd8e0fa67695073e
arian <arian.maki@yahoo.com>
2025-08-19 21:49:56 +0200

    getting the data from API

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,25 @@ import CharacterList from "./components/CharacterList";
 import CharacterDetails from "./components/CharacterDetails";
 import Navbar from "./components/Navbar";
 import { allCharacters } from "../data/data";
+import { useEffect, useState } from "react";
 
 function App() {
+  const [characters, setCharacters] = useState([]);
+
+  useEffect(() => {
+    async function getCharacters() {
+      const res = await fetch("https://rickandmortyapi.com/api/character");
+      const data = await res.json();
+      setCharacters(data.results);
+    }
+    getCharacters();
+  }, []);
+
   return (
     <div className="app">
-      <Navbar />
+      <Navbar resultCount={characters.length} />
       <div className="main">
-        <CharacterList allCharacters={allCharacters} />
+        <CharacterList allCharacters={characters} />
         <CharacterDetails />
       </div>
     </div>
