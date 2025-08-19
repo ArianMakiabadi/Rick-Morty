2bb4f3362b47ec3eeb263ce7c95b460b5c2f3879
arian <arian.maki@yahoo.com>
2025-08-19 21:50:37 +0200

    made resultCount responsive

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,13 +1,13 @@
 import { HeartIcon } from "@heroicons/react/24/outline";
 
-function Navbar() {
+function Navbar({ resultCount }) {
   return (
     <nav className="navbar">
       <div className="navbar__logo">
         <img src="rick-sanchez.png" />
       </div>
       <input type="text" className="text-field" placeholder="Search..." />
-      <div className="navbar__result">Found x characters</div>
+      <div className="navbar__result">Found {resultCount} characters</div>
       <button className="heart">
         <HeartIcon className="icon" />
         <span className="badge">4</span>
