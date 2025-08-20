05f84a6d11d2672fbd4ae2bbc2760566609353a3
arian <arian.maki@yahoo.com>
2025-08-20 23:19:35 +0200

    refactor: getCharacters() uses axios | error handling with toast

--- src/App.jsx
+++ src/App.jsx
@@ -2,23 +2,32 @@ import "./App.css";
 import CharacterList from "./components/CharacterList";
 import CharacterDetails from "./components/CharacterDetails";
 import Navbar from "./components/Navbar";
-import { allCharacters } from "../data/data";
 import { useEffect, useState } from "react";
+import axios from "axios";
+import toast, { Toaster } from "react-hot-toast";
 
 function App() {
   const [characters, setCharacters] = useState([]);
 
   useEffect(() => {
     async function getCharacters() {
-      const res = await fetch("https://rickandmortyapi.com/api/character");
-      const data = await res.json();
-      setCharacters(data.results);
+      try {
+        const { data } = await axios.get(
+          "https://rickandmortyapi.com/api/character"
+        );
+        setCharacters(data.results);
+      } catch (err) {
+        toast.error(err.response.data.error);
+      }
     }
     getCharacters();
   }, []);
 
   return (
     <div className="app">
+      <div>
+        <Toaster />
+      </div>
       <Navbar resultCount={characters.length} />
       <div className="main">
         <CharacterList allCharacters={characters} />
