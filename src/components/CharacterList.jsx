e66237924998b5a9573957ecc88489e7a6606f6e
arian <arian.maki@yahoo.com>
2025-09-08 13:07:00 +0200

    refactor: CharacterList using Tailwind | made responsive

--- src/components/CharacterList.jsx
+++ src/components/CharacterList.jsx
@@ -1,8 +1,6 @@
-import { Mars, Venus } from "lucide-react";
-
 function CharacterList({ allCharacters, setSelectedId }) {
   return (
-    <div className="Characters-list">
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6 mx-auto flex-1 ">
       {allCharacters.map((item) => (
         <Character key={item.id} item={item} setSelectedId={setSelectedId} />
       ))}
@@ -14,21 +12,27 @@ export default CharacterList;
 
 function Character({ item, setSelectedId }) {
   return (
-    <div className="list__item" onClick={() => setSelectedId(item.id)}>
-      <img src={item.image} alt={item.name} />
-      <h3 className="name">
-        <span>
-          {item.gender === "Male" ? (
-            <Mars stroke="#1E90FF" style={{ verticalAlign: "middle" }} />
-          ) : (
-            <Venus stroke="#FF69B4" style={{ verticalAlign: "middle" }} />
-          )}
-        </span>
-        <span> {item.name}</span>
+    <div
+      className="shadow-lg bg-slate-800 hover:bg-slate-700 p-4 rounded-3xl hover:scale-105 transition-transform duration-200 cursor-pointer flex flex-col sm:block"
+      onClick={() => setSelectedId(item.id)}
+    >
+      <img
+        className="rounded-full sm:rounded-3xl  mx-auto mb-2"
+        src={item.image}
+        alt={item.name}
+      />
+      <h3 className="mx-auto">
+        <span className="text-2xl text-slate-200"> {item.name}</span>
       </h3>
-      <div className="list-item__info">
+      <div className="mx-auto text-slate-400">
         <span
-          className={`status ${item.status === "Dead" ? "red" : ""}`}
+          className={`status ${
+            item.status === "Dead"
+              ? "bg-rose-600"
+              : item.status === "Alive"
+              ? "bg-green-600"
+              : "bg-yellow-400"
+          }`}
         ></span>
         <span> {item.status}</span>
         <span> - {item.species}</span>
