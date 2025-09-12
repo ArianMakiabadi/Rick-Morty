425c7aeb9a070677c29d05c3d50d6a9a434b4ee1
arian <arian.maki@yahoo.com>
2025-09-13 01:52:10 +0200

    font-size fixes

--- src/components/CharacterDetails.jsx
+++ src/components/CharacterDetails.jsx
@@ -51,22 +51,22 @@ function CharacterDetails({
       onClick={() => setSelectedId(null)}
     >
       <div
-        className="max-w-[80%] max-h-[90%] w-full rounded-3xl overflow-hidden bg-slate-800"
+        className="max-w-[80%] max-h-[90%] w-full rounded-3xl flex flex-col bg-slate-800"
         onClick={(e) => e.stopPropagation()} // prevent closing when clicking inside
       >
         {/* Character Info Section */}
-        <div className="flex flex-col lg:flex-row justify-center items-center lg:justify-start lg:gap-10 overflow-hidden p-4">
+        <div className="flex flex-col lg:flex-row justify-center items-center lg:justify-start lg:gap-10 overflow-hidden p-4 pb-0">
           <img
-            className="w-60 h-60 rounded-full"
+            className="w-40 lg:w-60 rounded-full"
             src={selectedCharacter.image}
             alt={selectedCharacter.name}
           />
           <div className="flex flex-col justify-center items-center lg:items-start gap-2 p-4">
-            <h3 className="flex items-center  text-white text-3xl text-nowrap">
+            <h3 className="flex items-center text-white text-xl lg:text-3xl text-nowrap">
               <span>{genderIcons[selectedCharacter.gender]}</span>
               <span>&nbsp;{selectedCharacter.name}</span>
             </h3>
-            <div className="text-slate-200">
+            <div className="text-slate-200 text-xs lg:text-sm">
               <span
                 className={`status ${
                   selectedCharacter.status === "Dead"
@@ -79,23 +79,30 @@ function CharacterDetails({
               <span>&nbsp;{selectedCharacter.status}</span>
               <span> -&nbsp;{selectedCharacter.species}</span>
             </div>
-            <p className="text-slate-300 ">
-              <span className="font-extrabold">Last known location: </span>
-              <span>{selectedCharacter.location.name}</span>
-            </p>
-            <p className="text-slate-300 mb-6">
-              <span className="font-extrabold">Origin: </span>
-              <span>{selectedCharacter.origin.name}</span>
-            </p>
+            <div className="flex flex-col flex-wrap gap-x-4 text-slate-300 text-sm lg:text-base">
+              <p className="text-center lg:text-left">
+                <span className="font-extrabold">Last known location: </span>
+                <span className="inline-block">
+                  {selectedCharacter.location.name}
+                </span>
+              </p>
+              <p className="text-center lg:text-left">
+                <span className="font-extrabold">Origin: </span>
+                <span className="inline-block">
+                  {selectedCharacter.origin.name}
+                </span>
+              </p>
+            </div>
+
             <div className="text-slate-400">
               {isFavorite ? (
-                <p className="flex flex-row">
-                  <Check stroke="#28A745" style={{ verticalAlign: "middle" }} />
+                <p className="flex flex-row text-xs">
+                  <Check stroke="#28A745" className="w-4 pb-1" />
                   &nbsp;This character is added to favorites
                 </p>
               ) : (
                 <button
-                  className="btn btn--primary"
+                  className="btn btn--primary text-xs"
                   onClick={() => onAddFavorite(selectedCharacter)}
                 >
                   Add to favourite
@@ -105,31 +112,28 @@ function CharacterDetails({
           </div>
         </div>
         {/* Divider */}
-        <hr className="h-[1px] bg-gray-600 rounded-3xl my-0.5 mx-6"></hr>
+        <hr className="h-[1px] bg-gray-600 rounded-3xl my-0.5 lg:mt-4 mx-6"></hr>
 
         {/* Episodes Section */}
-        <div className=" p-4 ">
+        <div className=" p-4 pt-2 flex-1 min-h-0 flex flex-col">
           <div className="flex items-center justify-between mb-2">
-            <h2 className="text-slate-400 ">
+            <h2 className="text-slate-400 text-xs">
               Episodes Featuring {selectedCharacter.name}:
             </h2>
             <button>
-              <ArrowUpCircleIcon
-                className="w-8 h-8 text-slate-300 transition-all duration-300 ease-in-out
-"
-              />
+              <ArrowUpCircleIcon className="w-6 text-slate-300 transition-all duration-300 ease-in-out" />
             </button>
           </div>
-          <ul className="grid grid-cols-1 sm:grid-cols-2 gap-3 max-h-52 overflow-auto pr-2">
-            {episodes.map((item, index) => (
+          <ul className="grid grid-cols-1 sm:grid-cols-2 gap-3 max-h-40 overflow-y-auto pr-2">
+            {episodes.map((item) => (
               <li
-                className="bg-gray-700 rounded-3xl p-3 shadow hover:bg-gray-600 transition"
+                className="bg-gray-700 rounded-xl p-3 shadow hover:bg-gray-600 transition"
                 key={item.id}
               >
-                <div className="text-slate-200">
+                <div className="text-slate-200 text-xs lg:text-sm">
                   {item.episode} : <strong>{item.name}</strong>
                 </div>
-                <div className="text-xs text-gray-400 mt-1">
+                <div className="text-[0.6rem] lg:text-xs text-gray-400 mt-0.5">
                   Air Date: {item.air_date}
                 </div>
               </li>
